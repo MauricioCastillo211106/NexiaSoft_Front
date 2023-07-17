af20bacca0725cb68557d4d901575e2bd83999e6
Mauricio David Castillo Ruvalcaba <maurix.cas.ruv11@gmail.com>
2023-07-16 23:32:31 -0600

    modification folder & Graphics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Datos from './pages/HomePage';
+import Tablas from './pages/tabla';
+import Login from './pages/login';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+      <Routes>
+        <Route exact path="/" element={ <Login/>} />
+        <Route exact path="/Datos" element={ <Datos/>} />
+        <Route exact path="/Tablas" element={ <Tablas/>} />
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/pages/login.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import "../assets/styles/login.css"
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      email: email,
+      password: password,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("https://nexiasoftpi-production.up.railway.app/api/user/login", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        
+        console.log(result)
+        navigate("/Datos");
+      }
+      )
+      .catch((error) => console.log("error", error));
+  };
+
+  return (
+    <div className="container-fluid login-container">
+       <div className="form-container">
+      <h1 className="text-center">NexiaSoft</h1>
+      <form className="mx-auto" onSubmit={handleSubmit}>
+        <h4 className="text-center">Bienvenido</h4>
+        <div className="imagen"></div>
+        <div className="mb-3 mt-5">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Usuario
+          </label>
+          <input
+            type="email"
+            placeholder="Ingresa el usuario"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            placeholder="Ingrese la contraseña"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary mt-5">
+          Iniciar sesión
+        </button>
+      </form>
+      <h5>Sistema de riego inteligente para agricultura de precisión</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
